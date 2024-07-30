708c273d329b4d8a0b61224e5ce873e4e90a12be
Minwoo Yu <snowypainter@gmail.com>
2024-07-31 06:07:45 +0900

    Error Report: Pipeline Solution -> Numpy upgrade -> mojito2 can't use

--- LM/news.py
+++ LM/news.py
@@ -1,5 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+
+def preprocess_text(texts):
+    processed_texts = []
+    for text in texts:
+        text = re.sub(r'\s+', ' ', text)  # 여러 공백을 하나의 공백으로
+        text = re.sub(r'[^가-힣\s]', '', text)  # 한글과 공백을 제외한 문자 제거
+        processed_texts.append(text.strip())  # 양쪽 공백 제거
+    return processed_texts
 
 def get_page(symbol, n):
     url = f"https://finance.naver.com/item/news_news.naver?code={symbol}&page={n}&clusterId="
